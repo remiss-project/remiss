67ebd2806f45d93a14abc95e5ee2ab03152ba782
Carles R. Riera <blauigris@gmail.com>
2023-09-18 18:59:15 +0200

    Add support for generating an explicit preprocessed file with the timestamps in the format expected by mongodbimport to become dates.

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import plotly.express as px
 import pandas as pd
 import os
 
-from dash import dcc
+from dash import dcc, Dash, callback, Output, Input
 from dash import html
 from pymongo import MongoClient
 import pymongoarrow.monkey
 from pymongoarrow.api import Schema
+
+# import data with mongoimport first
+# $ mongoimport --db test_remiss --collection <collection> --file <dataset>.preprocessed.jsonl --drop
 
 app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL])
 server = app.server
@@ -17,55 +20,60 @@ pymongoarrow.monkey.patch_all()
 
 client = MongoClient("localhost", 27017)
 database = client.get_database("test_remiss")
-collection = database.get_collection("test_tweets")
+available_datasets = database.list_collection_names()
+client.close()
 
-colors = {"background": "#FFFFFF", "text": "#1DA1F2"}
+# Initialize the app - incorporate a Dash Bootstrap theme
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-pymongoarrow.monkey.patch_all()
+# App layout
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div('Remiss', className="text-primary text-center fs-3")
+    ]),
 
-client = MongoClient("localhost", 27017)
-database = client.get_database("test_remiss")
-collection = database.get_collection("test_tweets")
-boundaries = list(range(0, 10000, 100))
-df = collection.aggregate_pandas_all([
-    {'$project': {
-        'retweet_count': '$public_metrics.retweet_count',
-    }},
-    {'$bucket': {
-        'groupBy': '$retweet_count',
-        'boundaries': boundaries,
-        'default': 'Other',
-        'output': {
-            'count': {'$sum': 1},
-        }
-    }
-    }
-],
-    schema=Schema({'_id': int, 'count': int})
+    dbc.Row([
+        dbc.RadioItems(options=[{"label": x, "value": x} for x in available_datasets],
+                       value='test_tweets',
+                       inline=True,
+                       id='radio-buttons-dataset')
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(figure={}, id='temporal-evolution')
+        ], width=6),
+    ]),
+
+], fluid=True)
+
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='temporal-evolution', component_property='figure'),
+    Input(component_id='radio-buttons-dataset', component_property='value')
 )
-# df['bins'] = boundaries[1:]
-fig = px.bar(df, x='_id', y="count")
+def update_graph(dataset_chosen):
+    client = MongoClient("localhost", 27017)
+    database = client.get_database("test_remiss")
+    collection = database.get_collection(dataset_chosen)
+    # get available fields
+    # available_fields = collection.find_one()
+    df = collection.aggregate_pandas_all(
+        [
+            {'$group': {'_id': {'$dayOfYear': '$created_at'},
+                        'count': {'$sum': 1}}}
+        ],
+        schema=Schema({'_id': int, 'count': int})
+    )
+    fig = px.histogram(df, x='continent', y=dataset_chosen, histfunc='avg')
+    return fig
 
-fig.update_layout(
-    plot_bgcolor=colors["background"],
-    paper_bgcolor=colors["background"],
-    font_color=colors["text"],
-)
 
-app.layout = html.Div(
-    style={"backgroundColor": colors["background"]},
-    children=[
-        html.H1(
-            children="Histogram of Retweets",
-            style={"textAlign": "center", "color": colors["text"]},
-        ),
-        html.Div(
-            children="An example using Dash and MongoDB to display the retweet histogram.",
-            style={"textAlign": "center", "color": colors["text"]},
-        ),
-        dcc.Graph(id="example-twitter", figure=fig),
-    ],
-)
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
