112729c1052a23101c24eb90919fd5cc11360b8f
Carles R. Riera <blauigris@gmail.com>
2023-10-25 13:28:49 +0200

    Add cache support at app.py

--- app.py
+++ app.py
@@ -9,16 +9,30 @@ from figures import TweetUserPlotFactory, EgonetPlotFactory
 REMISS_MONGODB_HOST = os.environ.get('REMISS_MONGODB_HOST', 'localhost')
 REMISS_MONGODB_PORT = int(os.environ.get('REMISS_MONGODB_PORT', 27017))
 REMISS_MONGODB_DATABASE = os.environ.get('REMISS_MONGODB_DATABASE', 'remiss')
+REMISS_CACHE_DIR = os.environ.get('REMISS_CACHE_DIR', None)
 
-# Run the app
-if __name__ == '__main__':
+def main():
+    print(f'Connecting to MongoDB at {REMISS_MONGODB_HOST}:{REMISS_MONGODB_PORT}...')
+    print(f'Using database {REMISS_MONGODB_DATABASE}...')
+    if REMISS_CACHE_DIR:
+        print(f'Using cache directory {REMISS_CACHE_DIR}...')
+    else:
+        print('Not using cache...')
+
+    print('Creating plot factories...')
     tweet_user_plot_factory = TweetUserPlotFactory(host=REMISS_MONGODB_HOST, port=REMISS_MONGODB_PORT,
                                                    database=REMISS_MONGODB_DATABASE)
     egonet_plot_factory = EgonetPlotFactory(host=REMISS_MONGODB_HOST, port=REMISS_MONGODB_PORT,
-                                            database=REMISS_MONGODB_DATABASE)
+                                            database=REMISS_MONGODB_DATABASE, cache_dir=REMISS_CACHE_DIR)
     dashboard = RemissDashboard(tweet_user_plot_factory, egonet_plot_factory)
-
+    print('Creating app...')
     app = dash.Dash(external_stylesheets=[dbc.themes.JOURNAL])
     app.layout = dashboard.layout()
     dashboard.callbacks(app)
+    print('Running app...')
     app.run(debug=True)
+
+
+# Run the app
+if __name__ == '__main__':
+    main()
