f43c61c9200d0ace8ff4e2d02d4f874ff89afcce
Carles Riera <blauigris@gmail.com>
2024-05-31 13:57:37 +0200

    Fix prepopulate

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ available_theme_css = {'BOOTSTRAP': dbc.themes.BOOTSTRAP,
                        }
 
 
-def prepopulate(config):
+def prepopulate(config_file='dev_config.yaml'):
+    config = load_config(config_file)
     egonet_plot_factory = EgonetPlotFactory(host=config['mongodb']['host'], port=config['mongodb']['port'],
                                             cache_dir=config['cache_dir'],
                                             layout=config['graph_layout'],
