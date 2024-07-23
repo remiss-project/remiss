c86d52cf19451c074b9866f11ec86ff4fcdc5cec
Carles Riera <blauigris@gmail.com>
2024-07-23 12:50:23 +0200

    Treure slider dates

--- components/egonet.py
+++ components/egonet.py
@@ -14,7 +14,6 @@ class EgonetComponent(RemissComponent):
         super().__init__(name=name)
         self.debug = debug
         self.dates = None
-        self.frequency = plot_factory.frequency
         self.plot_factory = plot_factory
         self.state = state
         self.available_datasets = plot_factory.available_datasets
@@ -28,11 +27,8 @@ class EgonetComponent(RemissComponent):
                                           # value=available_users[0],
                                           id=f'user-dropdown-{self.name}', disabled=True)
         self.depth_slider = dcc.Slider(min=1, max=5, step=1, value=2, id=f'slider-{self.name}')
-        self.date_slider = dcc.Slider(min=0, max=1, step=1, id=f'date-slider-{self.name}', included=False)
-        self.user_checkbox = dcc.Checklist(options=[{"label": " Filter by user", "value": 1}], value=[],
+        self.user_checkbox = dcc.Checklist(options=[{"label": " Show egonet", "value": 1}], value=[],
                                            id=f'user-checkbox-{self.name}')
-        self.date_checkbox = dcc.Checklist(options=[{"label": " Filter by date", "value": 1}], value=[],
-                                           id=f'date-checkbox-{self.name}')
 
     def layout(self, params=None):
         return dbc.Card([
@@ -59,57 +55,32 @@ class EgonetComponent(RemissComponent):
                     ], style={'padding': '0px 50px 50px 0px'})
                 ], id=f'collapse-user-{self.name}', is_open=False),
 
-                dbc.Row([
-                    dbc.Col([
-                        self.date_checkbox
-                    ], width=6),
-                ]),
-                dbc.Collapse([
-                    dbc.Row([
-                        dbc.Col([
-                            self.date_slider
-                        ], width=12),
-                    ], style={'padding': '0px 50px 50px 0px'})
-                ], id=f'collapse-date-{self.name}', is_open=False),
                 html.Div([], style={'height': '100%'}, id=f'placeholder-{self.name}') if self.debug else None,
             ])
         ], style={'height': '100%'})
 
-    def update(self, dataset, egonet_user, user, state_start_date, state_end_date, hashtag, depth, date_index,
-               user_disabled,
-               depth_disabled, date_disabled):
+    def update(self, dataset, egonet_user, user, start_date, end_date, hashtags, user_disabled, depth):
         logger.info(f'Updating egonet with dataset {dataset}, user {user}, egonet user {egonet_user}, '
-                    f'start date {state_start_date}, end date {state_end_date}, hashtag {hashtag}, depth {depth}, '
-                    f'date index {date_index}, user disabled {user_disabled}, depth disabled {depth_disabled}, '
-                    f'date disabled {date_disabled}')
+                    f'start date {start_date}, end date {end_date}, hashtag {hashtags}, depth {depth}, '
+                    f'user disabled {user_disabled}')
         if user_disabled:
             egonet_user = None
-        if depth_disabled:
-            depth = None
-
-        if date_disabled:
-            # if the date slider is disabled then pick the one from the main date picker
-            start_date = state_start_date
-            end_date = state_end_date
-        else:
-            start_date = self.dates[date_index]
-            end_date = self.dates[date_index + 1]
 
         if ctx.triggered_id == f'user-dropdown-{self.name}' or ctx.triggered_id == f'slider-{self.name}':
             # Show egonet for the selected user
             try:
-                fig = self.plot_factory.plot_egonet(dataset, egonet_user, depth, start_date, end_date, hashtag)
+                fig = self.plot_factory.plot_egonet(dataset, egonet_user, depth, start_date, end_date, hashtags)
                 title = f'Egonet'
                 logger.info(f'Plotting egonet for user {egonet_user}')
             except (RuntimeError, ValueError, KeyError) as e:
                 # If the user is not available, then show the backbone
                 fig = self.plot_factory.plot_hidden_network(dataset, start_date=start_date, end_date=end_date,
-                                                            hashtag=hashtag)
+                                                            hashtag=hashtags)
                 title = 'Filtered network'
                 logger.info(f'User {egonet_user} not available, plotting backbone')
         else:
             # Plot backbone but highlight the selected user
-            fig = self.plot_factory.plot_hidden_network(dataset, user, start_date, end_date, hashtag)
+            fig = self.plot_factory.plot_hidden_network(dataset, user, start_date, end_date, hashtags)
             title = 'Filtered network'
             logger.info(f'Plotting backbone with table user {user} highlighted')
         # remove margin
@@ -123,16 +94,6 @@ class EgonetComponent(RemissComponent):
     def update_current_date(self, date):
         logger.info(f'Updating egonet date with {date}')
         return date
-
-    def update_date_slider(self, start_date, end_date):
-        days = pd.date_range(start_date, end_date, freq=self.frequency)
-        style = {'transform': 'rotate(45deg)', "white-space": "nowrap", 'text-align': 'center', 'font-size': '12px',
-                 'margin-top': '1rem'}
-        marks = {i: {'label': str(days[i].date()), 'style': style} for i in
-                 range(0, len(days))}
-        self.dates = days
-
-        return 0, len(days) - 1, 0, marks
 
     def update_user_checkbox(self, checkbox_value):
         return not checkbox_value, not checkbox_value
@@ -151,42 +112,30 @@ class EgonetComponent(RemissComponent):
         available_users.columns = ['label', 'value']
         return available_users.to_dict('records')
 
-    def update_debug(self, dataset, user, depth, date_index, user_checkbox, date_checkbox,
-                     user_dropdown_disabled, depth_slider_disabled, date_slider_disabled):
-        return f'Dataset: {dataset}, User: {user}, Depth: {depth}, Date index: {date_index}, ' \
-               f'User checkbox: {user_checkbox}, Date checkbox: {date_checkbox}, ' \
-               f'User dropdown disabled: {user_dropdown_disabled}, Depth slider disabled: {depth_slider_disabled}, ' \
-               f'Date slider disabled: {date_slider_disabled}'
+    def update_debug(self, dataset, user, depth, user_checkbox,
+                     user_dropdown_disabled, depth_slider_disabled):
+        return f'Dataset: {dataset}, User: {user}, Depth: {depth}, ' \
+               f'User checkbox: {user_checkbox},  ' \
+               f'User dropdown disabled: {user_dropdown_disabled}, Depth slider disabled: {depth_slider_disabled}, '
 
     def callbacks(self, app):
         app.callback(
-            Output(self.graph_egonet, 'figure'),
+            Output(f'fig-{self.name}', 'figure'),
             Output(f'title-{self.name}', 'children'),
             [Input(self.state.current_dataset, 'data'),
-             Input(self.user_dropdown, 'value'),
+             Input(f'user-dropdown-{self.name}', 'value'),
              Input(self.state.current_user, 'data'),
              Input(self.state.current_start_date, 'data'),
              Input(self.state.current_end_date, 'data'),
              Input(self.state.current_hashtags, 'data'),
-             Input(self.depth_slider, 'value'),
-             Input(self.date_slider, 'value'),
              Input(self.user_dropdown, 'disabled'),
-             Input(self.depth_slider, 'disabled'),
-             Input(self.date_slider, 'disabled'),
-             ],
+             Input(self.depth_slider, 'value')],
         )(self.update)
         app.callback(
             Output(self.user_dropdown, 'value'),
             [Input(self.state.current_user, 'data')],
         )(self.update_user)
-        app.callback(
-            Output(self.date_slider, 'min'),
-            Output(self.date_slider, 'max'),
-            Output(self.date_slider, 'value'),
-            Output(self.date_slider, 'marks'),
-            [Input(self.state.current_start_date, 'data'),
-             Input(self.state.current_end_date, 'data')],
-        )(self.update_date_slider)
+
         app.callback(
             Output(self.user_dropdown, 'options'),
             [Input(self.state.current_dataset, 'data')],
@@ -196,18 +145,11 @@ class EgonetComponent(RemissComponent):
             Output(self.depth_slider, 'disabled'),
             Input(self.user_checkbox, 'value'),
         )(self.update_user_checkbox)
-        app.callback(
-            Output(self.date_slider, 'disabled'),
-            Input(self.date_checkbox, 'value'),
-        )(self.update_date_checkbox)
+
         app.callback(
             Output(f'collapse-user-{self.name}', 'is_open'),
             Input(self.user_checkbox, 'value'),
         )(self.update_user_collapse)
-        app.callback(
-            Output(f'collapse-date-{self.name}', 'is_open'),
-            Input(self.date_checkbox, 'value'),
-        )(self.update_date_collapse)
 
         if self.debug:
             app.callback(
@@ -215,11 +157,8 @@ class EgonetComponent(RemissComponent):
                 [Input(self.state.current_dataset, 'data'),
                  Input(self.state.current_user, 'data'),
                  Input(self.depth_slider, 'value'),
-                 Input(self.date_slider, 'value'),
                  Input(self.user_checkbox, 'value'),
-                 Input(self.date_checkbox, 'value'),
                  Input(self.user_dropdown, 'disabled'),
                  Input(self.depth_slider, 'disabled'),
-                 Input(self.date_slider, 'disabled'),
                  ]
             )(self.update_debug)
